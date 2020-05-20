2ca8907752f4d46c77e55bd745e1ee5907e18cc4
B Kleinen <kleinen@infrastructure.de>
2020-05-20 16:00:23 +0200

    first components in pluspoller

--- src/pluspoller/pluspoller.js
+++ src/pluspoller/pluspoller.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 
 function PlusPoller(props){
     return(
+        <div className="d-flex p-2">
         <ul className="list-group">
             <li className="list-group-item">Cras justo odio</li>
             <li className="list-group-item">Dapibus ac facilisis in</li>
@@ -12,7 +13,16 @@ function PlusPoller(props){
             <li className="list-group-item">Porta ac consectetur ac</li>
             <li className="list-group-item">Vestibulum at eros</li>
         </ul>
+            <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                    <div className="input-group-text">
+                        <input type="checkbox" aria-label="Checkbox for following text input"/>
+                    </div>
+                </div>
+                <input type="text" className="form-control" aria-label="Text input with checkbox"/>
+            </div>
+            I'm a flexbox container!</div>
     )
-
 }
+const options =[{name: "Vanilla", checked: false},{name: "Chocolate, checked: false"}]
 ReactDOM.render(<PlusPoller/>,document.getElementById('pluspoller'))
