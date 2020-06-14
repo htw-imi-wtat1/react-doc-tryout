3339e6aec20c3f9da446d83f2ddbc0eee75d616a
B Kleinen <kleinen@infrastructure.de>
2020-06-15 01:09:06 +0200

    wip

--- src/pluspoller/pluspoller.js
+++ src/pluspoller/pluspoller.js
@@ -2,27 +2,38 @@ import 'bootstrap';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+function Option(props){
+    return(
+    <div className="input-group mb-3">
+        <div className="input-group-prepend">
+            <div className="input-group-text">
+                <input type="checkbox"   />
+            </div>
+        </div>
+        <input type="text" className="form-control"  value = {props.option.label} readOnly = {props.readOnly}/>
+    </div>
+)
+}
 
 function PlusPoller(props){
+    const newOption = {checked: false}
     return(
-        <div className="d-flex p-2">
-        <ul className="list-group">
-            <li className="list-group-item">Cras justo odio</li>
-            <li className="list-group-item">Dapibus ac facilisis in</li>
-            <li className="list-group-item">Morbi leo risus</li>
-            <li className="list-group-item">Porta ac consectetur ac</li>
-            <li className="list-group-item">Vestibulum at eros</li>
-        </ul>
-            <div className="input-group mb-3">
-                <div className="input-group-prepend">
-                    <div className="input-group-text">
-                        <input type="checkbox" aria-label="Checkbox for following text input"/>
-                    </div>
-                </div>
-                <input type="text" className="form-control" aria-label="Text input with checkbox"/>
-            </div>
-            I'm a flexbox container!</div>
+        <div className="container">
+            <h2>{props.poll.question}</h2>
+            { props.poll.options.map((option) => {
+                     return(<Option key={option.name} option = {option} readOnly={true} />)
+            })
+            }
+            <Option key="newOption" option = {newOption} readOnly={false} />
+        </div>
     )
 }
-const options =[{name: "Vanilla", checked: false},{name: "Chocolate, checked: false"}]
-ReactDOM.render(<PlusPoller/>,document.getElementById('pluspoller'))
+// poll:
+const poll =
+{ question: 'Please choose an icecream flavor!',
+    options:[{name: "vanilla", label: "Vanilla", checked: false},
+             {name: "chocolate", label: "Chocolate", checked: false},
+             {name: "pistachio", label: "Pistachio", checked: false}
+    ]}
+
+ReactDOM.render(<PlusPoller poll={poll}/>,document.getElementById('pluspoller'))
